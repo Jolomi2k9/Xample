b04124c4981c2c05c5273c635ef0cc97c94cfbc3
Jolomi <Jolomi@example.com>
2021-05-02 18:28:55 +0100

    Prevent user from inputing user id

--- crud/index.php
+++ crud/index.php
@@ -82,7 +82,11 @@ $_SESSION;
     </div>  
   
   </main>
+
+  
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+<script src="../crud/php/main.js"></script>
 </body>
 </html>
 
